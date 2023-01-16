0577795651f2292cfd72016e9b70bc84026d23df
BorisKlco <klco@hotmail.sk>
2023-01-16 17:59:27 +0100

    vsTest2

--- main.py
+++ main.py
@@ -1,39 +1,26 @@
 import pygame
 from sys import exit
-"""
-Content:
-|x| Vars,style,settings
-  |x| Game assets import
-  |x| Classes
-    |x| x class
-|x| Event loop
-  |x| fixed .blit assets
-  |x| logic .blit assets
-
-VSTEST
-"""
 
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
-pygame.display.set_caption('Jumpie')
+pygame.display.set_caption("Jumpie")
 clock = pygame.time.Clock()
-font = pygame.font.Font('assets/font/Pixeltype.ttf', 50)
+font = pygame.font.Font("assets/font/Pixeltype.ttf", 50)
 mainColor = "#c0e8ec"
 scoreColor = (64, 64, 64)
 playerGrav = 0
 jumpCount = 0
 
-skySur = pygame.image.load('assets/Sky.png').convert()
-groundSur = pygame.image.load('assets/ground.png').convert()
+skySur = pygame.image.load("assets/Sky.png").convert()
+groundSur = pygame.image.load("assets/ground.png").convert()
 
-score = font.render('Jumpie', False, scoreColor)
+score = font.render("Jumpie", False, scoreColor)
 scoreBox = score.get_rect(center=(400, 50))
 
-snailSur = pygame.image.load('assets/snail/snail1.png').convert_alpha()
+snailSur = pygame.image.load("assets/snail/snail1.png").convert_alpha()
 snailBox = snailSur.get_rect(bottomleft=(800, 300))
 
-playerSur = pygame.image.load(
-    'assets/player/player_walk_1.png').convert_alpha()
+playerSur = pygame.image.load("assets/player/player_walk_1.png").convert_alpha()
 playerBox = playerSur.get_rect(midbottom=(75, 300))
 
 while True:
@@ -47,13 +34,13 @@ while True:
                     playerGrav = -4
                     jumpCount += 1
 
-    #baseAssets
+    # baseAssets
     screen.blit(skySur, (0, 0))
     screen.blit(groundSur, (0, 300))
     pygame.draw.rect(screen, mainColor, scoreBox)
     screen.blit(score, scoreBox)
 
-    #gameAssets
+    # gameAssets
     snailBox.x -= 4
     if snailBox.right <= 0:
         snailBox.left = 800
