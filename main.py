2a93c4a9027105352902c27c32457ea9fda72d19
Boris Klco <122160506+BorisKlco@users.noreply.github.com>
2023-01-17 09:49:00 +0000

    Game active status

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ clock = pygame.time.Clock()
 font = pygame.font.Font("assets/font/Pixeltype.ttf", 50)
 mainColor = "#c0e8ec"
 scoreColor = (64, 64, 64)
+gameActive = True
 playerGrav = 0
 jumpCount = 0
 
@@ -20,7 +21,8 @@ scoreBox = score.get_rect(center=(400, 50))
 snailSur = pygame.image.load("assets/snail/snail1.png").convert_alpha()
 snailBox = snailSur.get_rect(bottomleft=(800, 300))
 
-playerSur = pygame.image.load("assets/player/player_walk_1.png").convert_alpha()
+playerSur = pygame.image.load(
+    "assets/player/player_walk_1.png").convert_alpha()
 playerBox = playerSur.get_rect(midbottom=(75, 300))
 
 while True:
@@ -29,30 +31,39 @@ while True:
             pygame.quit()
             exit()
         if event.type == pygame.KEYDOWN:
-            if jumpCount < 2:
+            if gameActive == True:
+                if jumpCount < 2:
+                    if event.key == pygame.K_SPACE:
+                        playerGrav = -4
+                        jumpCount += 1
+            else:
                 if event.key == pygame.K_SPACE:
-                    playerGrav = -4
-                    jumpCount += 1
+                    gameActive = True
 
-    # baseAssets
-    screen.blit(skySur, (0, 0))
-    screen.blit(groundSur, (0, 300))
-    pygame.draw.rect(screen, mainColor, scoreBox)
-    screen.blit(score, scoreBox)
+    if gameActive == True:
+        screen.blit(skySur, (0, 0))
+        screen.blit(groundSur, (0, 300))
+        pygame.draw.rect(screen, mainColor, scoreBox)
+        screen.blit(score, scoreBox)
 
-    # gameAssets
-    snailBox.x -= 4
-    if snailBox.right <= 0:
-        snailBox.left = 800
-    screen.blit(snailSur, snailBox)
+        snailBox.x -= 4
+        if snailBox.right <= 0:
+            snailBox.left = 800
+        screen.blit(snailSur, snailBox)
 
-    playerGrav += 0.20
-    playerBox.y += playerGrav
-    if playerBox.bottom >= 300:
-        playerGrav = 0
-        jumpCount = 0
-        playerBox.bottom = 300
-    screen.blit(playerSur, playerBox)
+        playerGrav += 0.20
+        playerBox.y += playerGrav
+        if playerBox.bottom >= 300:
+            playerGrav = 0
+            jumpCount = 0
+            playerBox.bottom = 300
+        screen.blit(playerSur, playerBox)
+
+        if snailBox.colliderect(playerBox):
+            gameActive = False
+            snailBox.left = 800
+    else:
+        screen.fill(mainColor)
 
     pygame.display.update()
     clock.tick(60)
